7ae6c6fc6d156c2568b2ae0817814a492d2b53cd
unknown <mprotasiewicz@softfusion.pl>
2019-07-03 11:57:47 +0200

    update

--- src/components/ProductItem/ProductItem.test.js
+++ src/components/ProductItem/ProductItem.test.js
@@ -3,14 +3,87 @@ import { shallow, mount, render } from 'enzyme';
 import { MockedProvider } from 'react-apollo/test-utils';
 
 import ProductItem from './ProductItem';
+import ProductItemList from './ProductItemList';
 
 import Mocks from '../../Mocks';
 
+const ProductList = {
+  products: [
+    {
+      name: 'Havoline Magnetic',
+      subname: 'EVO 15W-40',
+      description: 'Olej przekładniowy',
+      price: '149,00 zł',
+      oldPrice: '189,00 zł',
+      discount: '-15%',
+      value: '5l',
+      more:
+        'Najwyższej jakości, w pełni syntetyczny olej silnikowy do najnowszych samochodów benzynowych i Diesel. Olej dedykowany do najnowszych silników pojazdów grupy VW/Audi. Sprzedawca Petrostar. Oficjalna aprobata VW 504.00/507.00.'
+    },
+    {
+      name: 'Havoline Magnetic',
+      subname: 'EVO 15W-40',
+      description: 'Olej przekładniowy',
+      oldPrice: null,
+      discount: '15%',
+      price: '15,00 zł',
+      value: '5l',
+      more: 'Szczegółowy opis produktu'
+    },
+    {
+      name: 'Havoline Magnetic',
+      subname: 'EVO 15W-40',
+      description: 'Olej dniowy',
+      oldPrice: '125,00 zł',
+      discount: '-95%',
+      price: '15,00 zł',
+      value: '5l',
+      more: 'Szczegółowy opis produktu'
+    },
+    {
+      name: 'Magnetic',
+      subname: '15W-40',
+      description: 'Olej przekładniowy',
+      oldPrice: null,
+      discount: '15%',
+      price: '155,00 zł',
+      value: '5l',
+      more: 'Szczegółowy opis produktu'
+    },
+    {
+      name: 'Havoline Magnetic Magnetic',
+      subname: 'EVO ',
+      description: 'Olej przekładniowy',
+      oldPrice: '150,00 zł',
+      discount: '15%',
+      price: '15,00 zł',
+      value: '5l',
+      more: 'Szczegółowy opis produktu'
+    },
+    {
+      name: 'Havoline Magnetic',
+      subname: 'EVO 15W-40',
+      description: 'Olej przekładniowy',
+      oldPrice: null,
+      discount: '15%',
+      price: '15,00 zł',
+      value: '5l',
+      more: 'Szczegółowy opis produktu'
+    },
+    {
+      name: 'Havoline Magnetic',
+      subname: null,
+      description: 'Olej przekładniowy',
+      oldPrice: null,
+      discount: '15%',
+      price: '15,00 zł',
+      value: '5l',
+      more: 'Szczegółowy opis produktu'
+    }
+  ]
+};
+
 describe('<Product Item />', () => {
-  it('Init', () => {
-    const wrapper = shallow(<ProductItem />);
-    expect(wrapper);
-  });
   it('With data', () => {
     const wrapper = shallow(
       <ProductItem
@@ -60,11 +133,27 @@ describe('<Product Item />', () => {
     expect(wrapper);
   });
   it('With mock data', () => {
-    const withMock = mount(
+    const withMock = shallow(
       <MockedProvider mocks={Mocks} addTypename={false}>
-        <ProductItem />
+        <ProductItem data={{
+          subname: 'EVO 15W-40',
+          description: 'Olej przekładniowy',
+          oldPrice: '189,00 zł',
+          discount: '-90%',
+          price: '15,00 zł',
+          value: '5l',
+          more: 'Szczegółowy opis produktu'
+        }}/>
       </MockedProvider>
     );
     expect(withMock);
+  });
+  it('With mock data list', () => {
+    const withMockList = shallow(
+      <MockedProvider mocks={Mocks} addTypename={false}>
+        <ProductItemList products={ProductList.products}></ProductItemList>
+      </MockedProvider>
+    );
+    expect(withMockList);
   });
 });
